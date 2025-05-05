ef0e3d7ed5fc16e10e96c5a735c69898811a6f5d
Kiến Đức Trọng <kienductrong@gmail.com>
2025-05-05 14:37:35 +0700

    add webrtc

--- src/index.js
+++ src/index.js
@@ -10,11 +10,14 @@ import { cacheRouter } from "./routes/cacheRouter.js";
 import { mediaRouter } from "./routes/mediaRouter.js";
 import { embeddingRouter } from "./routes/embeddingRouter.js";
 import dbConfig from "./config/dbConfig.js";
+import { Server } from "socket.io";
+import { setupSocket } from "./config/socketHandler.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
 const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: corsOptions });
 
 app.use(cors(corsOptions));
 app.use(express.json({ limit: "1000mb" }));
@@ -36,3 +39,4 @@ httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 dbConfig();
+setupSocket(io);
